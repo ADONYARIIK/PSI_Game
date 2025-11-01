237571d8adaeba76c3859ef8e1ec98996b9937ff
ADONYARIK <scityrchenko@gmail.com>
2025-11-01 17:06:06 +0100

    Adding assets

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -8,6 +8,13 @@ export default class GameScene extends Phaser.Scene {
     create() {
         this.scene.launch('UIScene');
         this.scene.bringToTop('UIScene');
+
+        this.registry.set('hp', 1);
+        this.registry.set('coins', 0);
+        this.registry.set('scores', 0);
+        this.registry.set('level', 0);
+        this.registry.set('playerLength', 3);
+
     }
 
     update() {
--- src/scenes/UIScene.js
+++ src/scenes/UIScene.js
@@ -6,7 +6,7 @@ export default class UIScene extends Phaser.Scene {
     }
 
     create() {
-
+        
     }
 
     update() {
