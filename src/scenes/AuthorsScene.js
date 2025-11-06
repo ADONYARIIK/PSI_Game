e0ad1133c0a9a6dddf9ced1d602776353b57c41e
Tymofii Sova <timsova2004@gmail.com>
2025-11-06 23:07:13 +0100

    добавленны первые таблички

--- src/scenes/AuthorsScene.js
+++ src/scenes/AuthorsScene.js
@@ -7,6 +7,9 @@ export default class AuthorsScene extends Phaser.Scene {
 
     create(){
         const bg = this.add.image(0,0,"walls").setOrigin(0).setScale(1.1);
+        const sign = this.add.image(200,200,"sign").setOrigin(0).setScale(0.1);
+        const sign2 = this.add.image(700,200,"sign").setOrigin(0).setScale(0.1);
+        
     }
 
     update(){
