a6e8c415891ba9444f49b59fd34b25d2abec7457
Tymofii Sova <timsova2004@gmail.com>
2025-11-06 23:03:34 +0100

    добавленны основвные спрайты для авторской сцены и фон

--- src/scenes/AuthorsScene.js
+++ src/scenes/AuthorsScene.js
@@ -6,10 +6,10 @@ export default class AuthorsScene extends Phaser.Scene {
     }
 
     create(){
-
+        const bg = this.add.image(0,0,"walls").setOrigin(0).setScale(1.1);
     }
 
     update(){
-        
+
     }
 }
