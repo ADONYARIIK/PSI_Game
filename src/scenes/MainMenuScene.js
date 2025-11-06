601288fc5cd49843a809c4c4b12e9192275b1133
Tymofii Sova <timsova2004@gmail.com>
2025-11-06 20:21:17 +0100

    кнопка старта получила свою анимацию и теперь при нажатии переносит в GameScene

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -6,13 +6,42 @@ export default class MainMenuScene extends Phaser.Scene {
     }
 
     create() {
-        //this.scene.start('GameScene');
+       
+         const bg = this.add.image(0, -200, "gui", "mainMenuBackground.png").setOrigin(0).setScale(1.1);
+        const gameName = this.add.image(200, -100, "gui", "mainMenuLogo.png").setOrigin(0).setScale(0.5);
+
+
+        let startBtn = this.add.image(460, 450, "startBtn").setOrigin(0).setScale(0.15).setInteractive({ useHandCursor: true });
+        startBtn.on('pointerover', () => {
+            this.scaleUpBtn(startBtn);
+        });
+        startBtn.on('pointerout',()=>{
+            this.scaleDownBtn(startBtn);
+        });
+        startBtn.on('pointerdown',()=>{
+            this.scene.start('GameScene');
+        });
     }
 
     update() {
-        const bg = this.add.image(0,-200,"gui","mainMenuBackground.png").setOrigin(0).setScale(1.1);
-        const gameName = this.add.image(200,-100,"gui","mainMenuLogo.png").setOrigin(0).setScale(0.5);
-
-        const startBtn = this.add.image(460,450,"startBtn").setOrigin(0).setScale(0.15).setInteractive({useHandCursor: true});
     }
+
+    scaleUpBtn(obj){
+        this.tweens.add({
+                targets: obj,
+                scale: 0.16,
+                duration: 150,
+                ease: 'Power1'
+            });
+    }
+    scaleDownBtn(obj){
+        this.tweens.add({
+                targets: obj,
+                scale: 0.15,
+                duration: 150,
+                ease: 'Power1'
+            });
+    }
+
+
 }
